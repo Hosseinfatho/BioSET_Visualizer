10d451a6d5dd72f19376897a44ee71b78005e403
Chahat Kalsi <chahat.ck88@gmail.com>
2026-02-01 06:55:42 -0500

    Channel list UI modified

--- src/bioset/ui/components/channels.py
+++ src/bioset/ui/components/channels.py
@@ -42,42 +42,43 @@ def channels_section(state, ctrl):
                             dense=True,
                             class_="ch-item",
                         ):
-                            with html.Div(
-                                class_="ch-icons",
-                                title=("channel.name",),
-                            ):
-                                with vuetify.VMenu(
-                                    offset_y=True,
-                                    close_on_content_click=False,
+                            with vuetify.VListItemIcon():
+                                with html.Div(
+                                    class_="ch-icons",
+                                    title=("channel.name",),
                                 ):
-                                    with html.Template(v_slot_activator="{ on, attrs }"):
-                                        vuetify.VIcon(
-                                            v_text="active_channels.includes(channel.id) ? 'mdi-square-rounded' : 'mdi-square-rounded-outline'",
-                                            style=("`color: ${channel.color || '#fff'}; cursor: pointer;`",),
-                                            v_bind="attrs",
-                                            v_on="on",
-                                            _class="mr-3",
-                                            click_stop=True,
-                                        )
-                                    
-                                    with vuetify.VCard():
-                                        vuetify.VColorPicker(
-                                            v_model=("channels[index].color",),
-                                            mode="hexa",
-                                            hide_mode_switch=True,
-                                            hide_inputs=True,
-                                            show_swatches=True,
-                                            swatches_max_height=150,
-                                            swatches=("color_swatches", []),
-                                            input=(ctrl.on_channel_color_change, "[channel.id, $event]"),
-                                        )
+                                    with vuetify.VMenu(
+                                        offset_y=True,
+                                        close_on_content_click=False,
+                                    ):
+                                        with html.Template(v_slot_activator="{ on, attrs }"):
+                                            vuetify.VIcon(
+                                                v_text="active_channels.includes(channel.id) ? 'mdi-square-rounded' : 'mdi-square-rounded-outline'",
+                                                style=("`color: ${channel.color || '#fff'}; cursor: pointer;`",),
+                                                v_bind="attrs",
+                                                v_on="on",
+                                                _class="mr-3",
+                                                click_stop=True,
+                                            )
+                                        
+                                        with vuetify.VCard():
+                                            vuetify.VColorPicker(
+                                                v_model=("channels[index].color",),
+                                                mode="hexa",
+                                                hide_mode_switch=True,
+                                                hide_inputs=True,
+                                                show_swatches=True,
+                                                swatches_max_height=150,
+                                                swatches=("color_swatches", []),
+                                                input=(ctrl.on_channel_color_change, "[channel.id, $event]"),
+                                            )
 
-                                vuetify.VIcon(
-                                    "mdi-close-circle",
-                                    v_if="active_channels.includes(channel.id)",
-                                    class_="eye-icon mr-3",
-                                    click_stop_prevent=(ctrl.toggle_channel, "[channel.id]"),
-                                )
+                                    vuetify.VIcon(
+                                        "mdi-close-circle",
+                                        v_if="active_channels.includes(channel.id)",
+                                        class_="eye-icon mr-3",
+                                        click_stop_prevent=(ctrl.toggle_channel, "[channel.id]"),
+                                    )
 
                             with vuetify.VListItemContent(v_if="!drawer_mini"):
                                 vuetify.VListItemTitle("{{ channel.name }}")
