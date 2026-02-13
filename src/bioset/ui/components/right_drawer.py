8ebdee27fd740cb0f66ebda69398982011eb8153
reichmla <lucareichmann01@gmail.com>
2026-02-13 19:29:08 +0100

    remove second upset container

--- src/bioset/ui/components/right_drawer.py
+++ src/bioset/ui/components/right_drawer.py
@@ -279,8 +279,6 @@ def right_drawer(state, ctrl):
                 />
                 """
             )
-            html.Div("Marker Combinations", classes="text-overline mb-3 text-center", style="color: white;")
-            html.Div(id="upset-container", style="width: 100%; height: 300px;")
             
         # Chatbot section
         chatbot_section(state, ctrl)
