95e6979f01a56f7f2f4f38b96f259faff08bca9b
reichmla <lucareichmann01@gmail.com>
2026-03-11 15:40:29 +0100

    fixed intensity slider

--- src/bioset/streaming/streamer.py
+++ src/bioset/streaming/streamer.py
@@ -73,6 +73,7 @@ class VolumeStreamer:
 
         self._channel_tfs: Dict[int,
                                 Tuple[vtkColorTransferFunction, vtkPiecewiseFunction]] = {}
+        self._channel_percentile_bounds: Dict[int, Tuple[float, float, float]] = {}
 
         self._pending_request: Optional[LoadRequest] = None
         self._debounce_lock = threading.Lock()
@@ -321,8 +322,9 @@ class VolumeStreamer:
         mapper.SetInputData(img)
         
         tint_rgb = self._channel_colors.get(channel_id, (1.0, 1.0, 1.0))
-        color_tf, opacity_tf = build_histogram_tf(img, tint_rgb=tint_rgb)
+        color_tf, opacity_tf, pct_range = build_histogram_tf(img, tint_rgb=tint_rgb)
         self._channel_tfs[channel_id] = (color_tf, opacity_tf)
+        self._channel_percentile_bounds[channel_id] = pct_range
         
         prop = vol.GetProperty()
         prop.SetColor(color_tf)
@@ -396,7 +398,8 @@ class VolumeStreamer:
         data_range = self._channel_data_range[channel_id]
         tint_rgb = self._channel_colors.get(channel_id, (1.0, 1.0, 1.0))
         
-        color_tf, opacity_tf = build_tf_with_range(data_range, range_pct, tint_rgb)
+        pct_bounds = self._channel_percentile_bounds.get(channel_id, (data_range[0], data_range[1], data_range[1]))
+        color_tf, opacity_tf = build_tf_with_range(data_range, pct_bounds, range_pct, tint_rgb)
         self._channel_tfs[channel_id] = (color_tf, opacity_tf)
         
         if channel_id in self.volumes:
